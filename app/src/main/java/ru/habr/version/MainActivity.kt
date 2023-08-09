a96736010c5c1645a6880a911adaf63bc5cd41d5
Sergey Lavrov <lavrov-sergey@yandex.ru>
2023-08-09 22:24:33 +0300

    Add app logic and UI to Main Activity

--- app/src/main/java/ru/habr/version/MainActivity.kt
+++ app/src/main/java/ru/habr/version/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.habr.version
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            textView.text = (1..6).random().toString()
+        }
     }
 }
