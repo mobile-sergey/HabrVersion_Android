9b9f9a70c0d11f7712d175f205a258fc47e3a6e6
Sergey Lavrov <lavrov-sergey@yandex.ru>
2023-08-09 23:15:03 +0300

    Prepare static LaunchActivity

--- app/src/main/java/ru/habr/version/LaunchActivity.kt
+++ app/src/main/java/ru/habr/version/LaunchActivity.kt
@@ -1,11 +1,28 @@
 package ru.habr.version
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
+@SuppressLint("CustomSplashScreen")
 class LaunchActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+
+        val duration = 2000L
+        val handler = Handler(Looper.myLooper()!!)
+
+        handler.postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            },
+            duration
+        )
     }
 }
