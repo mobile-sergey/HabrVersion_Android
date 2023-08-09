428777fe0cb0a8fe7a92308d9040f0cf75447346
Sergey Lavrov <lavrov-sergey@yandex.ru>
2023-08-09 20:23:06 +0300

    Add Launch Activity

---
+++ app/src/main/java/ru/habr/version/LaunchActivity.kt
@@ -0,0 +1,11 @@
+package ru.habr.version
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
